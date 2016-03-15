8e6eaad34042ad7a84717192511c55916fbbbfbb
Nicolas Roman <nicoroman@gmail.com>
2016-03-15 13:51:04 -0600

    Tarea completa

--- TareaVelocimetro/TareaVelocimetro.playground/Contents.swift
+++ TareaVelocimetro/TareaVelocimetro.playground/Contents.swift
@@ -1,16 +1,42 @@
-/* 
- TAREA VELOCIMETRO
- 
- Enumerar 4 Estados: Apagado, Velocidad Baja, Velocidad Media, Velocidad Alta
- Inicializar los estados
- 
- Clase Auto: 
-    Instancia de Estado
-    Variable entera "Estado Numerico"
-    Función que recibe objeto de la Enumeración, e imprime la velocidad a la que va
- 
- */
+// TAREA VELOCIMETRO
 
 import Cocoa
 
-        
+enum Velocidad : Int {
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    init(velocidadInicial : Velocidad) {
+        self = .Apagado
+    }
+}
+
+class Auto {
+    var velocidad : Velocidad
+    init(velocidad : Velocidad) {
+        self.velocidad = .Apagado
+    }
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String) {
+        if velocidad == .Apagado {
+            print("\(velocidad.rawValue), \(velocidad)")
+            velocidad = .VelocidadBaja
+        } else {
+            switch (velocidad) {
+            case .VelocidadBaja:
+                velocidad = .VelocidadMedia
+            case .VelocidadMedia:
+                velocidad = .VelocidadAlta
+            case .VelocidadAlta:
+                velocidad = .VelocidadMedia
+            default:
+                velocidad = .Apagado
+            }
+        }
+    return (velocidad.rawValue, "\(velocidad)")
+    }
+}
+
+var auto = Auto(velocidad: .Apagado)
+
+for n in 1...20 {
+    auto.cambioDeVelocidad()
+    print("\(auto.velocidad.rawValue), \(auto.velocidad)")
+}
