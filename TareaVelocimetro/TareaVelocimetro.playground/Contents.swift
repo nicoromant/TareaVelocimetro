d8c33c8bf7b0c14a7168fe095bec3c3764eee18d
Nicolas Roman <nicoroman@gmail.com>
2016-03-15 00:12:47 -0600

    Solo committed Contents.swift

---
+++ TareaVelocimetro/TareaVelocimetro.playground/Contents.swift
@@ -0,0 +1,16 @@
+/* 
+ TAREA VELOCIMETRO
+ 
+ Enumerar 4 Estados: Apagado, Velocidad Baja, Velocidad Media, Velocidad Alta
+ Inicializar los estados
+ 
+ Clase Auto: 
+    Instancia de Estado
+    Variable entera "Estado Numerico"
+    Función que recibe objeto de la Enumeración, e imprime la velocidad a la que va
+ 
+ */
+
+import Cocoa
+
+        
