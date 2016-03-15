d7b0936db4c363089b67514b106f2c52f3409ca8
Nicolas Roman <nicoroman@gmail.com>
2016-03-14 22:30:14 -0600

    Initial Commit

---
+++ TareaVelocimetroTests/TareaVelocimetroTests.swift
@@ -0,0 +1,36 @@
+//
+//  TareaVelocimetroTests.swift
+//  TareaVelocimetroTests
+//
+//  Created by Nicolas Roman Toro on 3/14/16.
+//  Copyright © 2016 Nicolas Roman Toro. All rights reserved.
+//
+
+import XCTest
+@testable import TareaVelocimetro
+
+class TareaVelocimetroTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
